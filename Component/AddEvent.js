3f0fe3ee67d0c041ed4be8fe79e461401ae3f323
Jelena <neohuilingjelena@gmail.com>
2018-06-07 15:12:21 +0800

    Handle event where fields are blank when adding event

--- Component/AddEvent.js
+++ Component/AddEvent.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Button
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Button
 } from 'react-native';
 
 import t from 'tcomb-form-native';
@@ -13,65 +13,75 @@ import realm from '../Database/AllSchemas';
 const Form = t.form.Form;
 
 const Event = t.struct({
-  name: t.String,
-  location: t.String,
-  type: t.String
+    name: t.String,
+    location: t.String,
+    type: t.String
 });
 
 const options = {
-  fields: {
-    name: {
-      error: 'You need a name for your event, idiot'
+    fields: {
+        name: {
+            error: 'You need a name for your event, idiot'
+        },
+        location: {
+            error: 'Choose a location...anywhere...'
+        },
+        type: {
+            error: 'whatkanda event isit?',
+        },
     },
-    location: {
-      error: 'Choose a location...anywhere...'
-    },
-    type: {
-      error: 'whatkanda event isit?',
-    },
-  },
 };
 
 export default class AddEvent extends Component {
 
-  static navigationOptions = {
-    header: null
-  }
+    static navigationOptions = {
+        header: null
+    };
 
-  handleSubmit = () => {
-    const value = this._form.getValue();
-    const event = {
-      id: Math.floor(Date.now() / 1000),
-      name: value.name,
-      location: value.location,
-      type: value.type
+    handleSubmit = () => {
+        const value = this._form.getValue();
+        if (!isEmpty(value)) {
+            const event = {
+                id: Math.floor(Date.now() / 1000),
+                name: value.name,
+                location: value.location,
+                type: value.type
+            }
+            realm.insertEvent(event);
+            this.props.navigation.navigate('Home');
+        }
     }
-    realm.insertEvent(event);
-    this.props.navigation.navigate('Home');
-  }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Form 
-          type={Event}
-          ref={c => this._form = c}
-          options={options}
-        />
-        <Button
-          title="Submit Event"
-          onPress={this.handleSubmit}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Form
+                    type={Event}
+                    ref={c => this._form = c}
+                    options={options}
+                />
+                <Button
+                    title="Submit Event"
+                    onPress={this.handleSubmit}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
-  },
+    container: {
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
+    },
 });
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+}
