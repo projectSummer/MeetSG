0f524b73f890d77c0f9ce89b8f442dedb74542d6
Khoo Wei Ping <khoowp25@hotmail.com>
2018-06-09 15:23:02 +0800

    Update index.js
    Ignore IsMounted() deprecated warning

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import { YellowBox } from 'react-native';
 
 AppRegistry.registerComponent('AwesomeProject', () => App);
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
