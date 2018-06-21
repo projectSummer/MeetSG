a5e00e1503bc325ba0671a11ef799610a29c3f74
LawMan <lawrenciok@gmail.com>
2018-06-22 01:22:19 +0800

    update options

--- Component/Login.js
+++ Component/Login.js
@@ -21,10 +21,10 @@ const User = t.struct({
 const options = {
   fields: {
     email: {
-      error: 'Without an email address how are you going to reset your password when you forget it?'
+      error: 'you need this to login moron.'
     },
     password: {
-      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+      error: 'lemme guess, you forgot?'
     },
   },
 };
